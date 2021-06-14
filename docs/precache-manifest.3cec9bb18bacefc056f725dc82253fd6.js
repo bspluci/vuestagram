0aa9c8977e4d6c45e81de2750afb144e3f6f2584
pjh <pjh@gmail.com>
2021-06-14 10:48:05 +0900

    aa

---
+++ docs/precache-manifest.3cec9bb18bacefc056f725dc82253fd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0988ce26767b158bfac1",
+    "url": "/vuestagram/css/app.78eaefce.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuestagram/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0e2563de04d9026bdd0f0006e5748187",
+    "url": "/vuestagram/index.html"
+  },
+  {
+    "revision": "0988ce26767b158bfac1",
+    "url": "/vuestagram/js/app.6bd69ff2.js"
+  },
+  {
+    "revision": "7a4760b8b42d11b796e9",
+    "url": "/vuestagram/js/chunk-vendors.6235989a.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/vuestagram/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vuestagram/robots.txt"
+  }
+]);
